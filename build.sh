d180ec7bff5cb2d82a065dd040ad1bf705b3630c
Luke Mondy <luke.mondy@data61.csiro.au>
2019-03-14 17:20:48 +1100

    Add sh script to build docker images

---
+++ build.sh
@@ -0,0 +1,245 @@
+#!/bin/sh
+
+############################################
+# env setup
+
+DOCKERHUB="${DOCKERHUB:-trustworthysystems/}"
+
+# Base images
+DEBIAN_IMG="${DEBIAN_IMG:-debian:buster}"
+BASETOOLS_IMG="${BASETOOLS_IMG:-base_tools}"
+
+# Core images
+SEL4_IMG="${SEL4_IMG:-sel4}"
+CAMKES_IMG="${CAMKES_IMG:-camkes}"
+L4V_IMG="${L4V_IMG:-l4v}"
+
+# Extra feature images
+RUST_IMG="${RUST_IMG:-sel4_rust}"
+CAMKES_VIS_IMG="${CAMKES_VIS_IMG:-camkes_vis}"
+BINARY_DECOMP_IMG="${BINARY_DECOMP_IMG:-binary_decomp}"
+
+# For images that are prebuilt
+PREBUILT_RISCV_IMG="${PREBUILT_RISCV_IMG:-prebuilt_riscv_compilers}"
+PREBUILT_CAKEML_IMG="${PREBUILT_CAKEML_IMG:-prebuilt_cakeml}"
+
+
+# Interactive images
+EXTRAS_IMG="extras"
+USER_IMG="user_img-$(whoami)"
+USER_BASE_IMG="${SEL4_IMG}"
+HOST_DIR="${HOST_DIR:-$(pwd)}"
+
+# Volumes
+DOCKER_VOLUME_HOME="${DOCKER_VOLUME_HOME:-$(whoami)-home}"
+DOCKER_VOLUME_ISABELLE="${DOCKER_VOLUME_ISABELLE:-$(whoami)-isabelle}"
+
+# Extra vars
+DOCKER_BUILD="docker build"
+DOCKER_FLAGS="--force-rm=true"
+INTERNAL="no"
+
+if test -z "$EXEC"; then
+	EXEC="bash"
+	DOCKER_RUN_FLAGS="${DOCKER_RUN_FLAGS} -it"
+fi
+
+###########################
+# For 'prebuilt' images, the idea is that for things that take a long
+# time to build, and don't change very much, we should build them 
+# once, and then pull them in as needed.
+USE_PREBUILT_RISCV="${USE_PREBUILT_RISCV:-yes}"
+RISCV_BASE_DATE="${RISCV_BASE_DATE:-2018_06_04}"
+USE_CAKEML_RISCV="${USE_CAKEML_RISCV:-yes}"
+CAKEML_BASE_DATE="${CAKEML_BASE_DATE:-2019_01_13}"
+
+
+############################################
+# builder functions
+
+build_internal_image()
+{
+    base_img="$1"
+    dfile_name="$2"
+    img_name="$3"
+    shift 3;
+    other_flags=$@
+
+    $DOCKER_BUILD $DOCKER_FLAGS \
+        --build-arg base_img="${base_img}" \
+        "${other_flags}" \
+        -f "${dfile_name}" \
+        -t "${img_name}" \
+        .
+}
+
+build_image()
+{
+    base_img="$1"
+    dfile_name="$2"
+    img_name="$3"
+    shift 3;
+    other_flags=$@
+
+    build_internal_image "${DOCKERHUB}${base_img}" "${dfile_name}" "${DOCKERHUB}${img_name}" $other_flags
+}
+
+apply_software_to_image()
+{
+    prebuilt_img="$1"
+    builder_dfile="$2"
+    orig_img="$3"
+    new_img="$4"
+    shift 4;
+    other_flags=$@
+
+    # NOTE: it's OK to supply docker build-args that aren't requested in the Dockerfile
+
+    $DOCKER_BUILD $DOCKER_FLAGS \
+		--build-arg BASE_BUILDER_IMG="${DOCKERHUB}${prebuilt_img}" \
+		--build-arg BASE_IMG="${DOCKERHUB}${orig_img}" \
+        ${other_flags} \
+		-f "${builder_dfile}" \
+		-t "${DOCKERHUB}${new_img}" \
+		.
+}
+############################################
+
+############################################
+# Recipes for standard images
+
+build_sel4()
+{
+    build_internal_image "$DEBIAN_IMG" base_tools.dockerfile "$BASETOOLS_IMG" "--build-arg INTERNAL=${INTERNAL}"
+    build_internal_image "$BASETOOLS_IMG" sel4.dockerfile "${DOCKERHUB}${SEL4_IMG}"
+}
+
+build_camkes()
+{
+    build_image ${SEL4_IMG} camkes.dockerfile ${CAMKES_IMG}
+    # Note here we apply the cakeml software, and overwrite the CAMKES_IMG tag
+    apply_software_to_image ${BASE_CAKEML_IMG} apply-cakeml.dockerfile ${CAMKES_IMG} ${CAMKES_IMG}
+}
+
+build_l4v()
+{
+    build_image ${CAMKES_IMG} l4v.dockerfile ${L4V_IMG}
+}
+
+############################################
+# Build prebuildable images
+
+# TODO insert building of prebuilt images
+
+############################################
+# Argparsing
+
+show_help()
+{
+    # TODO:
+    # - learn how to use sed better
+    # - learn best way to represent that -s can be supplied multiple times
+    available_software=$(find . -name 'apply-*.dockerfile' \
+                            | sed 's/.dockerfile//' \
+                            | sed 's/.\/apply-//' \
+                            | sort \
+                            | tr "\n" "|")
+    echo "run.sh [-r] -b [sel4|camkes|l4v] -s [${available_software}] -s ..."
+    echo ""
+    echo " -r     Rebuild docker images (don't use the docker cache)"
+    echo " -v     Verbose mode"
+    echo " -s     Strict mode"
+    echo ""
+    echo "Sneaky hints:"
+    echo " - You can actually run this with \`-b sel4-rust\`, or any other existing image,"
+    echo "   but it will ruin the sorting of the name."
+
+}
+
+# init cmdline vars to nothing
+img_to_build=
+img_to_run=
+software_to_apply=
+pull_base_first=
+
+while getopts "h?epvb:rs:" opt; do
+    case "$opt" in
+    h|\?)
+        show_help
+        exit 0
+        ;;
+    e)  strict=1
+        set -e
+        ;;
+    v)  verbose=1
+        set -x
+        ;;
+    b)  img_to_build=$OPTARG
+        ;;
+    p)  pull_base_first=y
+        ;;
+    r)  DOCKER_FLAGS="${DOCKER_FLAGS} --no-cache"
+        ;;
+    s)  software_to_apply="$software_to_apply $OPTARG"
+        ;;
+    :)  echo "Option -$OPTARG requires an argument." >&2
+        exit 1
+        ;;
+    esac
+done
+
+if test -z "$img_to_build"; then
+    echo "You need to supply a \`-b\`" >&2
+    show_help
+    exit 1
+fi
+
+
+
+############################################
+# Processing
+
+if test -z "$pull_base_first"; then
+    # If we don't want to pull the base image from Dockerhub, build it
+    "build_${img_to_build}"
+else
+    docker pull "${DOCKERHUB}${img_to_build}"
+fi
+
+# get a unique, sorted, space seperated list of software to apply.
+#softwares="$(echo $software_to_apply | sed 's/ /\n/g' | sort | uniq | sed 's/\n/ /g')"
+softwares="$(echo $software_to_apply | tr ' ' '\n' | sort | uniq | tr '\n' ' ')"
+
+base_img="$img_to_build"
+for s in $softwares; do
+    echo $s to install!
+    if test -f "apply-${s}.dockerfile"; then
+        # Try to resolve if we have a prebuilt image for the software being asked for.
+        # If not, <shrug />, docker won't pick up the variable anyway, so no harm done.
+        prebuilt_img="$(echo PREBUILT_${s}_IMG | tr [a-z] [A-Z])"
+        prebuilt_img="$(eval echo \$$prebuilt_img)"
+        apply_software_to_image "${prebuilt_img}" "apply-${s}.dockerfile" "$base_img" "${base_img}-${s}"
+        base_img="${base_img}-${s}"
+    fi
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
