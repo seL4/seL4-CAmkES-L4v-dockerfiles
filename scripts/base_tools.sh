8d38e3e29cc624228cf39dc7fea46479730f2cd4
TS Bamboo <bamboo@ts.data61.csiro.au>
2020-07-18 20:51:12 +1000

    [AUTO COMMIT] Update SNAPSHOT_DATE

--- scripts/base_tools.sh
+++ scripts/base_tools.sh
@@ -24,7 +24,7 @@ test -d "$DIR" || DIR=$PWD
 : "${SCM:=https://github.com}"
 
 # Debian Snapshot date
-: "${SNAPSHOT_DATE:=20200712}"
+: "${SNAPSHOT_DATE:=20200718}"
 
 if [ "$DESKTOP_MACHINE" = "no" ] ; then
 
