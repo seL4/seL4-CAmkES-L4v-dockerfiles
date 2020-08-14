7b485c05b81cc35ecf5c396c03c7065c0b1bc0f7
Luke Mondy <luke.mondy@data61.csiro.au>
2020-08-14 16:23:56 +1000

    Fix rust script, so any user can use the rust compiler.
    Signed-off-by: Luke Mondy <luke.mondy@data61.csiro.au>

--- scripts/apply-rust.sh
+++ scripts/apply-rust.sh
@@ -10,6 +10,7 @@ test -d "$DIR" || DIR=$PWD
 
 # tmp space for building 
 : "${TEMP_DIR:=/tmp}"
+: "${CARGO_HOME:=/etc/cargo}"
 
 # Not strictly necessary, but it makes the apt operations in
 # ../dockerfiles/apply-rust.dockerfile work.
@@ -18,11 +19,22 @@ as_root apt-get update -q
 try_nonroot_first mkdir -p "$TEMP_DIR" || chown_dir_to_user "$TEMP_DIR"
 # Get rust nightly
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > "$TEMP_DIR/rustup.sh"
+
+try_nonroot_first mkdir -p "$CARGO_HOME" || chown_dir_to_user "$CARGO_HOME"
+
 sh "$TEMP_DIR/rustup.sh" -y --default-toolchain nightly
 rm "$TEMP_DIR/rustup.sh"
 
+# Update the current shell
+# shellcheck disable=SC1090
+source "$CARGO_HOME"/env
+
 # Install rumprun target
-"$HOME/.cargo/bin/rustup" target add x86_64-rumprun-netbsd
+rustup target add x86_64-rumprun-netbsd
+
+# Make sure that all the files are accessible to other users:
+try_nonroot_first chmod -R o+rx "$CARGO_HOME"
 
 # Add cargo and rust to PATH
-echo "export PATH=\"\$PATH:\$HOME/.cargo/bin\"" >> "$HOME/.bashrc"
+echo "export CARGO_HOME=\"$CARGO_HOME\"" >> "$HOME/.bashrc"
+echo "export PATH=\"\$PATH:\$CARGO_HOME/bin\"" >> "$HOME/.bashrc"
