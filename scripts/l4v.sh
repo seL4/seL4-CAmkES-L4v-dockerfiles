ee33c88658a9519209416c68565c3c15b26f659f
Gerwin Klein <gerwin.klein@proofcraft.systems>
2024-02-10 11:35:33 +1100

    l4v: reduce l4v image size
    - use default polyml32_64 builds, which go up to 16GB of heap size,
      which was the max before anyway. This should reduce heap size.
    
    - remove vscodium component from cache (saves > 1GB)
    
    - remove separate MLTon download, which is now an Isabelle component
      anyway
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- scripts/l4v.sh
+++ scripts/l4v.sh
@@ -53,13 +53,6 @@ as_root apt-get install -y --no-install-recommends \
         python3-lxml \
         # end of list
 
-# looks like there is no Debian package for mlton any more
-MLTON=mlton-20210117-1.amd64-linux-glibc2.31
-wget https://github.com/MLton/mlton/releases/download/on-20210117-release/$MLTON.tgz
-tar  -xzC /opt -f $MLTON.tgz
-ln -s /opt/$MLTON opt/mlton
-rm $MLTON.tgz
-
 # Get l4v and setup isabelle
 try_nonroot_first mkdir "$ISABELLE_DIR" || chown_dir_to_user "$ISABELLE_DIR"
 ln -s "$ISABELLE_DIR" "$HOME/.isabelle"
@@ -67,6 +60,14 @@ mkdir -p "$HOME/.isabelle/etc"
 
 ISABELLE_SETTINGS_LOCATION="$HOME/.isabelle/etc/settings"
 cp "$NEW_ISABELLE_SETTINGS" "$ISABELLE_SETTINGS_LOCATION"
+
+# MLton is needed for the L4v C parser. It is a default component for Isabelle,
+# so we point PATH directly there. The component will only be available by
+# default if the cache below is installed or if the components are later
+# installed by the user. To run anything in L4v one has to install components,
+# so the path assignment here will be useful even without the cache.
+# The component name/version is stable enough to update manually when it changes.
+export PATH="/isabelle/contrib/mlton-20210117-1/x86_64-linux/bin/":"$PATH"
 
 if [ "$MAKE_CACHES" = "yes" ] ; then
     # Get a copy of the L4v repo, and build all the isabelle and haskell
@@ -77,6 +78,20 @@ if [ "$MAKE_CACHES" = "yes" ] ; then
         repo sync -c
         pushd l4v
             ./isabelle/bin/isabelle components -a
+
+            # Isabelle downloads tar.gz files, and then uncompresses them for
+            # its contrib. We don't need both the uncompressed AND decompressed
+            # versions, but Isabelle checks for the tarballs. To fool it, we now
+            # truncate the tars and save disk space. We also remove the large
+            # vscodium component, which is not needed for command line builds.
+            # This will lead to a warning when Isabelle starts, which is safe to
+            # ignore.
+            pushd "$HOME/.isabelle/contrib"
+                truncate -s0 ./*.tar.gz
+                rm -r vscodium-*
+                ls -lah  # show the evidence
+            popd
+
             pushd spec/haskell
                 make sandbox
             popd
@@ -86,14 +101,6 @@ if [ "$MAKE_CACHES" = "yes" ] ; then
     # Now cleanup the stuff we don't want cached
     rm -rf "$TEMP_L4V_LOCATION"
     as_root rm -rf /tmp/isabelle-  # This is a random tmp folder isabelle makes
-
-    # Isabelle downloads tar.gz files, and then uncompresses them for its contrib.
-    # We don't need both the uncompressed AND decompressed versions, but Isabelle
-    # checks for the tarballs. To fool it, we now truncate the tars and save disk space.
-    pushd "$HOME/.isabelle/contrib"
-        truncate -s0 ./*.tar.gz
-        ls -lah  # show the evidence
-    popd
 fi
 
 possibly_toggle_apt_snapshot
